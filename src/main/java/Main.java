03869ef89ba17c898e3bb41a26754d12525f625d
baruisoham <baruisoham@gmail.com>
2025-06-27 18:11:12 +0530

    test modularity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,15 +26,12 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
-      BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
-              Request newRequest = Request.getRequestFromBytes(in);
-              int correlationId = newRequest.getHeader().getCorrelationId();
-         
-     
+      BufferedInputStream inputStreamFromClient = new BufferedInputStream(clientSocket.getInputStream());
+      BufferedOutputStream outStreamToClient = new BufferedOutputStream(clientSocket.getOutputStream());
+      Request newRequest = StreamUtils.receiveRequest(inputStreamFromClient);
+      int correlationId = newRequest.getHeader().getCorrelationId();
       Response newResponse = new Response(0, new Header(correlationId));
-      out.write(newResponse.getResponseAsBytes());
-      out.flush(); // sometimes, when we want to write and send data, the data is still in the memory, not transferred to buffer. We use flush() to force the data to be sent immediately.
+      StreamUtils.sendResponse(outStreamToClient, newResponse); // to send response back to the client
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
